dd3d7cd0f2a946914de6bbe4e16ff1fa51a968e5
XeLeX <xelexis123@gmail.com>
2019-03-15 12:17:07 +0000

    [server] Migrate to ORM (closes #8)

--- chirper.js
+++ chirper.js
@@ -1,38 +1,74 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const mysql = require('mysql')
+const Sequelize = require('sequelize')
 
 require('dotenv').config()
 
 const port = process.env.CHIRPER_PORT
 
-const connection = mysql.createConnection({
-    host     : process.env.CHIRPER_DB_HOST,
-    user     : process.env.CHIRPER_DB_USER,
-    password : process.env.CHIRPER_DB_PASS,
-    database : process.env.CHIRPER_DB_NAME
+// Database //
+
+const sequelize = new Sequelize({
+    'host'     : process.env.CHIRPER_DB_HOST,
+    'port'     : process.env.CHIRPER_DB_PORT,
+    'database' : process.env.CHIRPER_DB_NAME,
+    'username' : process.env.CHIRPER_DB_USER,
+    'password' : process.env.CHIRPER_DB_PASS,
+    'dialect'  : 'mysql'
 })
+
+// const User = sequelize.define('user', {
+//     'login' : {
+//         'type' : Sequelize.STRING,
+//         'allowNull' : false,
+//         'unique' : true
+//     },
+//     'password' : {
+//         'type' : Sequelize.STRING,
+//         'allowNull' : false
+//     }
+// });
+
+const Chirp = sequelize.define('chirp', {
+    'content' : {
+        'type' : Sequelize.STRING,
+        'allowNull' : false
+    }
+});
+
+// User.hasMany(Chirp)
+// Chirp.belongsTo(User)
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true}))
 app.set('view engine', 'ejs')
 
 app.get('/', (_, response) => {
-    connection.query('SELECT * FROM chirps;', (error, results) => {
-        if (error) throw error
-        response.render('index', { 'chirps' : results })
+    Chirp.findAll().then(chirps => {
+        response.render('index', { 'chirps' : chirps })
+    }).catch(error => {
+        console.error(error)
+        response.status(500).end('Internal Server Error')
     })
 })
 app.post('/', (request, response) => {
-    const content = request.body.content
-    connection.query("INSERT INTO chirps SET ?;", { 'content' : content }, error => {
-        if (error) throw error
-        response.writeHead(302, {
-            'Location': '/'
-        });
-        response.end();
+    Chirp.create({
+        'content': request.body.content
+    }).then(chirp => {
+        response.redirect('/')
+    }).catch(error => {
+        console.error(error)
+        response.status(500).end('Intertal Server Error')
     })
 })
 
-app.listen(port, () => console.log(`The Chirper server is listening on port ${port}.`))
+sequelize.sync().then(() => {
+    // return User.create({
+    //     'login': 'user',
+    //     'password': process.env.CHIRPER_TEST_USER_PASS
+    // })
+}).then(() => {
+    app.listen(port, () => console.log(`The Chirper server is listening on port ${port}.`))
+});
+
